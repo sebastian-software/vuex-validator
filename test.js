e3db96c61c133f3227ecdc0aee72c802cc1202cb
Sebastian Fastner <s.fastner@sebastian-software.de>
2016-05-11 16:33:25 +0200

    Add simple test for lib

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
 /**
  * @license
- * VueLocale <https://www.sebastian-software.de/oss>
+ * VueXValidator <https://www.sebastian-software.de/oss>
  * Copyright 2015-2016 Sebastian Software GmbH
  * Released under Apache 2.0 <http://www.apache.org/licenses/LICENSE-2.0>
- * Authors: Sebastian Werner <s.werner@sebastian-software.de>
+ * Authors: Sebastian Fastner <s.fastner@sebastian-software.de>
  */
 
 import test from "ava"
 import "babel-register"
 
-import VueLocale from "./src/VuexValidator"
+import VueXValidator from "./src/VuexValidator"
+
+test("VueXValidator Plugin is valid", (api) =>
+{
+  api.is(typeof VueXValidator, "object")
+  api.is(typeof VueXValidator.install, "function")
+})
